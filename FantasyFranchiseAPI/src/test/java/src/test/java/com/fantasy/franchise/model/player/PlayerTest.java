da7472b7114867b29d122bf20cb5db6320ace44a
jruscito <jruscito@uri.edu>
2022-06-17 01:20:34 -0400

    new test file

--- FantasyFranchiseAPI/src/test/java/src/test/java/com/fantasy/franchise/model/player/PlayerTest.java
+++ FantasyFranchiseAPI/src/test/java/src/test/java/com/fantasy/franchise/model/player/PlayerTest.java
@@ -1,4 +1,12 @@
+package src.test.java.com.fantasy.franchise.model.player;
+
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PlayerTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
 }
